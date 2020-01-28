29700183d58af302f264fb8e28f167f80fe8f9c5
Deepan Reguraman <Deepan.Reguraman@centurylink.com>
2020-01-28 19:51:06 +0530

    more

--- testPython/com/dee/python/test/numpys/IndexAndSlicing.py
+++ testPython/com/dee/python/test/numpys/IndexAndSlicing.py
@@ -1,10 +1,31 @@
+#4:04:09
 import numpy as np
 
 npArray = np.arange(1, 27)
 indexNum = npArray[19]
-print("npArray:", npArray)
-print("Index(19):", indexNum, " Type Of npArray:", type(npArray), " Type Of Index:", type(indexNum))
+print("---Indexing---")
+print("npArray:", npArray, " Type Of npArray:", type(npArray))
+print("Index(19):", indexNum, " Type Of Index:", type(indexNum))
 indexNumAdd = indexNum + 6
 print("Index Addition:", indexNumAdd, " Type:", type(indexNumAdd))
+indAdd = indexNum + npArray[5]
+print("Another Index Add:", indAdd, "Type:", type(indAdd))
+print("Negative Indexing:", npArray[-1], npArray[25], npArray[-26], npArray[0])
+# print(npArray.slice(1, 12, 4))
 
-#print(npArray.slice(1, 12, 4))
+
+print("\n---Slicing---")
+arr_slice = slice(2, 26, 3) 
+slice_array = npArray[arr_slice]  # Starts with 2nd index, ends up till 26th index with step of 3 
+print("Slicing:", slice_array)
+print("Arranging into 2 dimensional:\n", slice_array.reshape(4, 2))
+print("Arranging into 3 dimensional:\n", slice_array.reshape(2, 2, 2))
+print("Between Slicing:", npArray[3:20])
+print("Before Slicing:", npArray[:20])
+print("After Slicing:", npArray[5:])
+
+
+print("\n---Slicing Multi Dimensional Array--")
+arrMd = np.array([1, 2, 3], [4, 5, 6], [7, 8, 9])
+print("0:2 x 23:25:\n", arrMd[0:2, 0:2])
+#print("0:2 x 13:15 x 23:25:\n", arrMd.reshape[0:2, :2, :2])
--- testPython/com/dee/python/test/numpys/basicNumpy.py
+++ testPython/com/dee/python/test/numpys/basicNumpy.py
@@ -6,7 +6,8 @@ numpyArray = np.array(singleDimArray)
 print("---Single Dimension Array---")
 print(singleDimArray, type(singleDimArray))
 print(numpyArray, type(numpyArray))
-print("arange:", np.arange(-10, 10))
+tenArray = np.arange(-10, 10)
+print("range:", tenArray)
 print("zeros:", np.zeros((3, 4)))  # Forms 3 rows x 4 columns multi dimensional zero array
 print("linspace:", np.linspace(6, 24, 4))  # Splitting 6 to 24 into 4 parts
 print("linspace:", np.linspace(0, 60, 11))  # Splitting 0 to 60 into 11 parts
@@ -20,11 +21,10 @@ print(numpyMultiArray, type(numpyMultiArray))
 mArray = np.zeros(8)
 arr3d = mArray.reshape(2, 2, 2)
 mRavel = arr3d.ravel()
-print("2x2x2:", arr3d)  # 3 Dimensional Array
+print("2x2x2:\n", arr3d)  # 3 Dimensional Array
 print("Ravel:", mRavel)
 print("4x2:", mArray.reshape(4, 2))
 print("8x1:", mArray.reshape(8, 1))
-tenArray = np.arange(-10, 10)
 print("5x4:", tenArray.reshape(5, 4))
 print("2x10:", tenArray.reshape(2, 10))
 toRavel = tenArray.reshape(10, 2)
