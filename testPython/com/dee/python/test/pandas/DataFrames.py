83c3b0d7379a5c7fdf9f59a25aeee34d713dac76
Deepan Reguraman <Deepan.Reguraman@centurylink.com>
2020-02-10 20:46:31 +0530

    more

--- testPython/com/dee/python/test/pandas/DataFrames.py
+++ testPython/com/dee/python/test/pandas/DataFrames.py
@@ -1,4 +1,4 @@
-#4:30:00
+# 4:34:14
 
 import pandas as pd
 
@@ -7,15 +7,36 @@ oneDTable = pd.DataFrame(oneDList)
 print(oneDTable)
 
 dict_var = [{'col1' : 1, 'col2': 2}, {'col1' : 20, 'col2' : 26, 'col3' : 71}, {'col1' : 50, 'col2' : 51}]
-dFrameDict = pd.DataFrame(dict_var, index = ['row1', 'row2', 'row3'])
+dFrameDict = pd.DataFrame(dict_var, index=['row1', 'row2', 'row3'])
 print(dFrameDict)
 
-
-stud1 = pd.Series([90, 95, 99], index = ['Physics', 'Chemistry', 'Mathematics'])
-stud2 = pd.Series([95, 98, 100, 100], index= ['Physics', 'Chemistry', 'Mathematics', 'Cse'])
+stud1 = pd.Series([90, 95, 99], index=['Physics', 'Chemistry', 'Mathematics'])
+stud2 = pd.Series([95, 98, 100, 100], index=['Physics', 'Chemistry', 'Mathematics', 'Cse'])
 marksTable = pd.DataFrame({
     "Dee" : stud1,
     "Pan" : stud2 
     })
-marksTable['Dp'] = pd.Series([90, 89, 95], index = ['Cse', 'Chemistry', 'Mathematics'])
+marksTable['Dp'] = pd.Series([90, 89, 95], index=['Cse', 'Chemistry', 'Mathematics'])
+print("\n---Print All Marks---\n", marksTable)
+
+print("\n---Add New Row---")
+addRow = pd.DataFrame([[70, 71], [72, 73]], columns=['Dee', 'Pan'])
+marksTable.append(addRow)
 print(marksTable)
+
+print("\n---Print Mathematics Row---\n")
+print(marksTable.loc['Mathematics'])
+
+print("\n---Print 0th Row---\n")
+print(marksTable.iloc[0])
+
+del(marksTable['Dp'])
+
+print("\n---After removing Dp---\n", marksTable)
+
+delEntry = marksTable.pop('Pan')
+print("\n---Popping Pan Entry---\n", delEntry)
+print("\n---After removing Pan---\n", marksTable)
+
+
+
