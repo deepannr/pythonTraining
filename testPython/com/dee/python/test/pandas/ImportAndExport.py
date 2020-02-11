056d29df4a74808443b13be6bf49bf54ea2baf3f
Deepan Reguraman <Deepan.Reguraman@centurylink.com>
2020-02-11 20:33:00 +0530

    more

--- testPython/com/dee/python/test/pandas/ImportAndExport.py
+++ testPython/com/dee/python/test/pandas/ImportAndExport.py
@@ -1,15 +1,18 @@
+#4:39:00
+
 import pandas as pd
 
-readCsvFile = pd.read_csv("C:/dev/projects/PythonTraining/Code/pythonTraining/testPython/com/dee/python/test/Fastag.csv")
+filesPath = "C:/dev/projects/PythonTraining/Code/pythonTraining/testPython/com/dee/python/test/tutorFiles/"
+
+readCsvFile = pd.read_csv(filesPath + "Fastag.csv")
 print("---Reading from Fastag.csv File---\n", readCsvFile)
-readCsvFile.to_csv("LocalFastag.csv")
+readCsvFile.to_csv(filesPath + "LocalFastag.csv")
 
-readCsvFile.to_excel("LocalFastag.xlsx")
-#readXlsFile = pd.read_excel("C:/dev/projects/PythonTraining/Code/pythonTraining/testPython/com/dee/python/test/Jan2020.xlsx")
-#print("\n---Reading xls file---\n", readXlsFile)
-#readXlsFile.to_excel("LocalJan2020.xls")
+readCsvFile.to_excel(filesPath + "LocalFastag.xlsx")
 
+readXlsFile = pd.read_excel(filesPath + "Jan2020.xlsx")
+print("\n---Reading xls file---\n", readXlsFile)
+readXlsFile.to_excel(filesPath + "LocalJan2020.xlsx")
 
-
-
-#pd.read_
+readJsonFile = pd.read_json(filesPath + "ci_vantive_network_devices.json")
+print("\n---Reading JSON file---\n", readJsonFile)
